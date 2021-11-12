b127138098a1844bfd528555850258688655c492
joellefkowitz <joellefkowitz@hotmail.com>
2021-11-12 15:40:50 -0500

    Type inner clients as explicitly indexable

--- src/caches/local.d.ts
+++ src/caches/local.d.ts
@@ -1,15 +1,9 @@
 declare module 'collections/lru-map' {
   class LruMap {
-    constructor(
-      values: any,
-      maxLength: any,
-      equals?: any,
-      hash?: any,
-      getDefault?: any
-    );
+    constructor(values: Record<string, string>, maxLength: number);
 
-    get<T>(key: any, defaultValue?: T): T;
-    set<T>(key: any, defaultValue?: T): LruMap;
+    get<T>(key: string, defaultValue?: T): T;
+    set<T>(key: string, defaultValue?: T): LruMap;
   }
 
   export default LruMap;
--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -3,8 +3,13 @@ import { AsyncCache, Cache, ImpureActions, PureActions } from '../main';
 import { LruCache } from '../caches/local';
 import { PrismaClient } from '@prisma/client';
 
+export type Transmutable = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+};
+
 export class Prisma {
-  private static _client: PrismaClient & { [index: string]: any };
+  private static _client: PrismaClient;
   private static _cache: Cache | AsyncCache;
 
   cacheFactory = (): Cache | AsyncCache => new LruCache(100);
@@ -16,6 +21,7 @@ export class Prisma {
   get client(): PrismaClient {
     if (!Prisma._client) {
       Prisma._client = new PrismaClient();
+
       this.cacheClientMethods();
     }
 
@@ -37,20 +43,23 @@ export class Prisma {
   }
 
   cacheClientMethods(): void {
+    const client: Transmutable = this.client;
+    const singleton: Transmutable = Prisma._client;
+
     for (const field of this.publicClientMethods) {
       for (const action of ImpureActions) {
-        const pristine = this.client[field][action];
+        const pristine = client[field][action];
 
-        this.client[field][action] = async (...args: unknown[]) => {
+        client[field][action] = async (...args: unknown[]) => {
           this.flushCache();
           return await pristine(...args);
         };
       }
 
       for (const action of PureActions) {
-        const pristine = this.client[field][action];
+        const pristine = client[field][action];
 
-        Prisma._client[field][action] = async (...args: unknown[]) => {
+        singleton[field][action] = async (...args: unknown[]) => {
           const key = JSON.stringify({ field, action, args });
           const read = await this.cache.read(key);
           const cached = read ? JSON.parse(read) : null;
