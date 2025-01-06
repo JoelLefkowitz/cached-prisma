ff1bdb9c029c377b1ec9bc84fd128df2c78d67ba
Digital <github@crni.xyz>
2025-01-06 16:55:35 +0100

    Update redis configurations to allow passing in different props

--- src/Redis.ts
+++ src/Redis.ts
@@ -1,14 +1,28 @@
 import { Cache } from "./Prisma";
-import { Redis as RedisClient } from "ioredis";
+import { Redis as RedisClient, RedisOptions } from "ioredis";
 
 export class Redis implements Cache {
   readonly lifetime: number;
 
   private client: RedisClient;
 
-  constructor(host = "0.0.0.0", port = 6379, lifetime = 10, prefix = "cache") {
-    this.client = new RedisClient(port, host, { keyPrefix: prefix });
-    this.lifetime = lifetime;
+  constructor(
+    hostOrRedisClient: string | RedisClient = "0.0.0.0",
+    portOrOptions: number | RedisOptions = 6379,
+    lifetimeSeconds = 60,
+    prefix = "cache"
+  ) {
+    if (typeof hostOrRedisClient === "string") {
+      if (typeof portOrOptions === "number") {
+        this.client = new RedisClient(portOrOptions, hostOrRedisClient, { keyPrefix: prefix });
+      } else {
+        this.client = new RedisClient(hostOrRedisClient, portOrOptions);
+      }
+    } else {
+      this.client = hostOrRedisClient;
+    }
+
+    this.lifetime = lifetimeSeconds;
   }
 
   read(key: string): Promise<string | null> {
